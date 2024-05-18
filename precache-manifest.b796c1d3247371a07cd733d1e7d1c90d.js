318e0013da22c541b39de595efc7defc3c7730cc
michal varyš <varys@varyshop.eu>
2024-05-18 17:16:47 +0000

    Updates

---
+++ precache-manifest.b796c1d3247371a07cd733d1e7d1c90d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1d174970efd7b9d3e3d0fc555f72a8",
+    "url": "/react-image-annotate/index.html"
+  },
+  {
+    "revision": "be4e878c76e38c7c91c2",
+    "url": "/react-image-annotate/static/css/main.c80f9f5f.chunk.css"
+  },
+  {
+    "revision": "8fd3b7286362dff780f3",
+    "url": "/react-image-annotate/static/js/2.9c55cff6.chunk.js"
+  },
+  {
+    "revision": "aca785b6b7b819a926e4fbbccd5bc0ac",
+    "url": "/react-image-annotate/static/js/2.9c55cff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4e878c76e38c7c91c2",
+    "url": "/react-image-annotate/static/js/main.50ae11d0.chunk.js"
+  },
+  {
+    "revision": "393b6d9263229f47bd2b",
+    "url": "/react-image-annotate/static/js/runtime-main.c7c62936.js"
+  }
+]);
